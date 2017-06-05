d4eb4565a7773d7b33ef4dc2868ea9cfa69caa4b
HantolR <HantolR@192.168.56.1>
2017-06-05 13:09:58 +0200

    using edit distance diagonal

--- evolutionary_strategy/src/(mu+lambda)-ES.h
+++ evolutionary_strategy/src/(mu+lambda)-ES.h
@@ -12,6 +12,7 @@
 #include "EditDistance.h"
 #include "MatchingSchema.h"
 
+#define CLOCKS_PER_MS (CLOCKS_PER_SEC / 1000)
 
 std::vector<ES_MatchingSchema> selectBestIndividuals(const unsigned mu,
 		std::vector<ES_MatchingSchema>& individuals, const bool plusSelection);
@@ -25,42 +26,51 @@ int evolutionStrategy(const std::vector<unsigned>& s1,
 		const size_t& p1, matching_schema<bool>& m, edit_distance& e,
 
 		const unsigned max_generations, const unsigned mu,
-		const unsigned lambda, const bool plusSelection)
+		const unsigned lambda)
 {
-	unsigned generation = 0;
+	clock_t start = clock();
+	long double msElapsed = 0;
 
 	ES_MatchingSchema startingMS(sig1, sig2);
 
+	ES_MatchingSchema best;
+	best.costValue = std::numeric_limits<unsigned int>::max();
+
 	//Generate mu random individuals
-	std::vector<ES_MatchingSchema> parents;
+	ES_MatchingSchema* const parents = new ES_MatchingSchema[mu + lambda];
 	for (unsigned i = 0; i < mu; ++i)
 	{
 		startingMS.shuffle();
 
-		//validate matching schema
-		if (ES_isValid(startingMS))
+		startingMS.costValue = e.edit_distance_matching_schema_enhanced(s1, s2,
+				s1l, s2l, startingMS.sigma1, startingMS.sigma2, sig1l, sig2l,
+				m);
+
+		parents[i] = startingMS;
+
+		if (parents[i].costValue < best.costValue)
 		{
-			startingMS.costValue =
-					e.edit_distance_matching_schema_enhanced(s1,
-							s2, s1l, s2l, startingMS.sigma1, startingMS.sigma2,
-							sig1l, sig2l, m);
-			parents.push_back(startingMS);
+			best = parents[i];
+
+			clock_t timeElapsed = clock() - start;
+			msElapsed = timeElapsed / CLOCKS_PER_MS;
+			std::cout << msElapsed << " " << best.costValue << "\n";
 		}
-		else
-		{
-			//TODO: not valid, maybe mutate until is valid?
-			//repeat iteration
-			i--;
-		}
+
 	}
 
+	const unsigned last = mu - 1;
+	std::sort(parents, parents + mu);
+
+	unsigned generation = 0;
 	while (generation <= max_generations)
 	{
+		unsigned childrenInPool = 0;
 		//Generate lambda children. Only mutation, no recombination
 		for (unsigned i = 0; i < lambda; i++)
 		{
 			//Choose random parent
-			unsigned p = rand() % mu;
+			const unsigned p = rand() % mu;
 
 			//Produce child, in the case parents=1 (like this) just clone
 			ES_MatchingSchema child = parents[p];
@@ -68,35 +78,59 @@ int evolutionStrategy(const std::vector<unsigned>& s1,
 			//mutate child
 			child.swap2();
 
-			//validate child
-			if (ES_isValid(child))
-			{
+			//select the worst parent: is the first element of the heap
+			const unsigned worstParentCostValue = parents[last].costValue;
+			int newDistance =
+					e.edit_distance_matching_schema_enhanced_with_diagonal(s1,
+							s2, s1l, s2l, child.sigma1, child.sigma2, sig1l,
+							sig2l, m, worstParentCostValue);
 
-				child.costValue =
-						e.edit_distance_matching_schema_enhanced(
-								s1, s2, s1l, s2l, child.sigma1, child.sigma2,
-								sig1l, sig2l, m);
-
-				parents.push_back(child);
-			}
-			else
+			if (newDistance != -1)
 			{
-				//TODO: not valid, maybe mutate until is valid?
-				//repeat iteration
-				i--;
+				//The child is better than the worst parent,
+				child.costValue = newDistance;
+
+				//so he is added to the pool
+				parents[mu + childrenInPool] = child;
+				childrenInPool++;
+
+//				//substitute the worst parent in the heap
+//				std::pop_heap(parents, parents + mu);
+//				parents[last] = child;
+//				std::push_heap(parents, parents + mu);
+
+				if (child.costValue < best.costValue)
+				{
+					best = child;
+
+					clock_t timeElapsed = clock() - start;
+					msElapsed = timeElapsed / CLOCKS_PER_MS;
+					std::cout << msElapsed << " " << best.costValue << "\n";
+				}
 			}
 		}
 
+		std::sort(parents, parents+mu+childrenInPool);
 
-		parents = selectBestIndividuals(mu, parents, plusSelection);
-
+//		std::cout<<"PARENTS:\n";
+//		for(int i=0; i<mu; i++)
+//		{
+//			 std::cout<<parents[i].costValue<<" ";
+//		}
+//		std::cout<<"\n";
+//		parents = selectBestIndividuals(mu, parents, plusSelection);
 
 		generation++;
 	}
 
-	//TODO return best of all
-	make_heap(parents.begin(), parents.end());
-	return parents.front().costValue;
+	clock_t timeElapsed = clock() - start;
+	msElapsed = timeElapsed / CLOCKS_PER_MS;
+	std::cout << msElapsed << " " << best.costValue << "\n";
+
+//	m.print_matching_schema(best.sigma1,best.sigma2);
+
+//	std::cout<<best.costValue;
+	return best.costValue;
 }
 
 std::vector<ES_MatchingSchema> selectBestIndividuals(const unsigned mu,
@@ -104,20 +138,14 @@ std::vector<ES_MatchingSchema> selectBestIndividuals(const unsigned mu,
 {
 	//TODO using a heap
 	std::vector<ES_MatchingSchema> bestIndividuals;
-	if (plusSelection)
-	{
-		make_heap(individuals.begin(), individuals.end());
 
-		for (unsigned i = 0; i < mu; i++)
-		{
-			pop_heap(individuals.begin(), individuals.end());
-			bestIndividuals.push_back(individuals.back());
-			individuals.pop_back();
-		}
-	}
-	else
+	make_heap(individuals.begin(), individuals.end());
+
+	for (unsigned i = 0; i < mu; i++)
 	{
-		//TODO Comma selection
+		pop_heap(individuals.begin(), individuals.end());
+		bestIndividuals.push_back(individuals.back());
+		individuals.pop_back();
 	}
 
 	return bestIndividuals;
